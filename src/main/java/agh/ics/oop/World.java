1d1cab0363c55ceea230b9a674954c6281133d6e
Krzysztof Pęczek <peczek.kr@gmail.com>
2022-11-03 10:35:13 +0100

    Final version of lab4 with tests

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -2,13 +2,10 @@ package agh.ics.oop;
 
 public class World {
     public static void main(String[] args) {
-        // MoveDirection[] directions = OptionsParser.parse(args);
-        // IWorldMap map = new RectangularMap(10, 5);
-        // Vector2d[] positions = { new Vector2d(2, 2), new Vector2d(3, 4) };
-        // IEngine engine = new SimulationEngine(directions, map, positions);
-        // engine.run();
-        Animal animal = new Animal();
-        animal.move(MoveDirection.FORWARD);
-        System.out.println(animal.getPosition());
+        MoveDirection[] directions = OptionsParser.parse(args);
+        IWorldMap map = new RectangularMap(10, 5);
+        Vector2d[] positions = { new Vector2d(2, 2), new Vector2d(3, 4) };
+        IEngine engine = new SimulationEngine(directions, map, positions);
+        engine.run();
     }
 }
---
+++ src/test/java/agh/ics/oop/SimulationTest.java
@@ -0,0 +1,36 @@
+package agh.ics.oop;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class SimulationTest {
+    private Animal animal;
+
+    @BeforeEach
+    public void setup() {
+        this.animal = new Animal();
+    }
+
+    @Test
+    public void test_movement() {
+        String[] args = "f b r l f f r r f f f f f f f f".split(" ");
+        MoveDirection[] directions = OptionsParser.parse(args);
+        IWorldMap map = new RectangularMap(10, 5);
+        Vector2d[] positions = { new Vector2d(2, 2), new Vector2d(3, 4) };
+        SimulationEngine engine = new SimulationEngine(directions, map, positions);
+        engine.run();
+
+        Animal animal_a = engine.getAnimal(0);
+        assertEquals(new Vector2d(2, 0), animal_a.getPosition());
+        assertEquals(MapDirection.SOUTH, animal_a.getOrientation());
+
+        Animal animal_b = engine.getAnimal(1);
+        assertEquals(new Vector2d(3, 4), animal_b.getPosition());
+        assertEquals(MapDirection.NORTH, animal_b.getOrientation());
+    }
+}
