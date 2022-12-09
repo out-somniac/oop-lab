ebb8f7cab02329386e6c7df7a2e7613e1646cd88
Krzysztof Pęczek <peczek.kr@gmail.com>
2022-12-09 21:42:21 +0100

    Added Vector and Direction class

---
+++ project/src/main/java/org/example/Direction.java
@@ -0,0 +1,75 @@
+package org.example;
+
+import org.example.Vector2d;
+
+public enum Direction {
+    NORTH,
+    NORTH_EAST,
+    EAST,
+    SOUTH_EAST,
+    SOUTH,
+    SOUTH_WEST,
+    WEST,
+    NORTH_WEST;
+
+    @Override
+    public String toString() {
+        return switch (this) {
+            case NORTH -> "N";
+            case NORTH_EAST -> "NE";
+            case EAST -> "E";
+            case SOUTH_EAST -> "SE";
+            case SOUTH -> "S";
+            case SOUTH_WEST -> "SW";
+            case WEST -> "W";
+            case NORTH_WEST -> "NW";
+        };
+    }
+
+    private static int toOrdinal(Direction direction) {
+        return switch (direction) {
+            case NORTH -> 0;
+            case NORTH_EAST -> 1;
+            case EAST -> 2;
+            case SOUTH_EAST -> 3;
+            case SOUTH -> 4;
+            case SOUTH_WEST -> 5;
+            case WEST -> 6;
+            case NORTH_WEST -> 7;
+        };
+    }
+
+    private Direction toEnum(int ordinal) {
+        return switch (ordinal) {
+            case 0 -> NORTH;
+            case 1 -> NORTH_EAST;
+            case 2 -> EAST;
+            case 3 -> SOUTH_EAST;
+            case 4 -> SOUTH;
+            case 5 -> SOUTH_WEST;
+            case 6 -> WEST;
+            case 7 -> NORTH_WEST;
+            default ->
+                throw new IllegalArgumentException(
+                        "Can not convert " + Integer.toString(ordinal) + " to type Direction.");
+        };
+    }
+
+    public Direction rotate(int rotation) {
+        return toEnum(toOrdinal(this) + rotation % 8);
+    }
+
+    public Vector2d toUnitVector() {
+        return switch (this) {
+            case NORTH -> new Vector2d(0, 1);
+            case NORTH_EAST -> new Vector2d(1, 1);
+            case EAST -> new Vector2d(1, 0);
+            case SOUTH_EAST -> new Vector2d(1, -1);
+            case SOUTH -> new Vector2d(0, -1);
+            case SOUTH_WEST -> new Vector2d(-1, -1);
+            case WEST -> new Vector2d(-1, 0);
+            case NORTH_WEST -> new Vector2d(-1, 1);
+
+        };
+    }
+}
---
+++ project/src/main/java/org/example/Vector2d.java
@@ -0,0 +1,62 @@
+package org.example;
+
+import java.util.Objects;
+
+public class Vector2d {
+    public final int x, y;
+
+    public Vector2d(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%s, %s)", this.x, this.y);
+    }
+
+    public boolean precedes(Vector2d other) {
+        return this.x <= other.x && this.y <= other.y;
+    }
+
+    public boolean follows(Vector2d other) {
+        return this.x >= other.x && this.y >= other.y;
+    }
+
+    public Vector2d upperRight(Vector2d other) {
+        return new Vector2d(Math.max(this.x, other.x), Math.max(this.y, other.y));
+    }
+
+    public Vector2d lowerLeft(Vector2d other) {
+        return new Vector2d(Math.min(this.x, other.x), Math.min(this.y, other.y));
+    }
+
+    public Vector2d add(Vector2d other) {
+        return new Vector2d(this.x + other.x, this.y + other.y);
+    }
+
+    public Vector2d subtract(Vector2d other) {
+        return new Vector2d(this.x - other.x, this.y - other.y);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+        if (other instanceof Vector2d that) {
+            return this.x == that.x && this.y == that.y;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+    public Vector2d opposite() {
+        return new Vector2d(-this.x, -this.y);
+    }
+}
