3e758f9eead1084549da1bc7b6f98655e0d2124d
Mikolaj Maslak <nicholasdev7@gmail.com>
2022-12-21 20:12:07 +0100

    Implemented genotype mixing

--- project/src/main/java/simulation/AnimalFactory.java
+++ project/src/main/java/simulation/AnimalFactory.java
@@ -1,20 +1,31 @@
 package simulation;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class AnimalFactory {
 
-    private final Configuration simulationConfig;
+    private final Configuration config;
 
     private final IMap map;
 
-    AnimalFactory(Configuration simulationConfig, IMap map) {
-        this.simulationConfig = simulationConfig;
+    AnimalFactory(Configuration config, IMap map) {
+        this.config = config;
         this.map = map;
     }
 
     public Animal createAnimal(Animal parent1, Animal parent2, int currentDay) {
         assert parent1.getPosition().equals(parent2.getPosition());
-        Animal newAnimal = new Animal(parent1.getPosition(), Direction.randomDirection(), map, simulationConfig, currentDay);
-        newAnimal.setEnergy(simulationConfig.getCreationEnergy() * 2);
+        Genotype newGenotype = createGenotype(parent1, parent2);
+        Animal newAnimal = new Animal(parent1.getPosition(), Direction.randomDirection(), map, config, newGenotype , currentDay);
+        newAnimal.setEnergy(config.getCreationEnergy() * 2);
         return newAnimal;
+    }
+
+    private Genotype createGenotype(Animal parent1, Animal parent2) {
+        Genotype newGenotype = ThreadLocalRandom.current().nextBoolean() ?
+                    Genotype.mixGenotypes(parent1.getGenotype(), parent2.getGenotype(), (double) parent1.energy / parent2.energy) :
+                    Genotype.mixGenotypes(parent2.getGenotype(), parent1.getGenotype(), (double) parent2.energy / parent1.energy);
+        newGenotype.mutateGenes(ThreadLocalRandom.current().nextInt(config.getMinMutations(), config.getMaxMutations() + 1), 1);
+        return newGenotype;
     }
 }
--- project/src/main/java/simulation/Genotype.java
+++ project/src/main/java/simulation/Genotype.java
@@ -2,9 +2,8 @@ package simulation;
 
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.stream.Collector;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -12,25 +11,27 @@ public class Genotype {
     public static final int MAX_GENE_VALUE = Direction.size - 1;
     public static final int MIN_GENE_VALUE = 0;
 
-    private final ArrayList<Integer> genes;
-    private final int genesCount;
+    private final List<Integer> genes;
     private Iterator<Integer> iter;
     private Integer currentGene;
 
     public Genotype(int genesCount) {
-        this.genesCount = genesCount;
         this.genes = getRandomGenes(genesCount);
         this.iter = genes.iterator();
         this.currentGene = iter.next();
     }
 
+    private Genotype(List<Integer> geneList) {
+        this.genes = geneList;
+        this.iter = genes.iterator();
+        this.currentGene = iter.next();
+    }
+
     public void advanceGene() {
-        if (iter.hasNext()) {
-            currentGene = iter.next();
-        } else {
+        if (!iter.hasNext()) {
             iter = genes.iterator();
-            currentGene = iter.next();
         }
+        currentGene = iter.next();
     }
 
     private int randomGene() {
@@ -45,5 +46,33 @@ public class Genotype {
 
     public int getRotation() {
         return currentGene;
+    }
+
+    public void mutateGenes(int nrOfMutations, int maxDifference) {
+        int n = this.genes.size();
+        for (int i = 0; i < nrOfMutations; i++) {
+            int k = ThreadLocalRandom.current().nextInt(0, this.genes.size());
+            this.genes.set(k, (this.genes.get(k) + n +
+                    ThreadLocalRandom.current().nextInt(-maxDifference, maxDifference + 1)) % n);
+        }
+    }
+
+
+    /**
+     * Function that creates new genotype from two other with ratio that tells
+     * what percentage of the new genome should come from each.
+     * It takes genes from right from gen1 and genes from left gen2
+     * @param gen1 first Genotype
+     * @param gen2 second Genotype
+     * @param firstToSecondRatio ratio of first genome to the second one
+     * @return new Genotype
+     */
+    static Genotype mixGenotypes(Genotype gen1, Genotype gen2, double firstToSecondRatio) {
+        double total = firstToSecondRatio + 1f;
+        int nrOfGenes1 = (int) Math.floor(gen1.genes.size() * (firstToSecondRatio / total));
+        int nrOfGenes2 = (int) Math.ceil(gen2.genes.size() * (1 - firstToSecondRatio / total));
+        List<Integer> geneList = new ArrayList<>(gen1.genes.subList(0, nrOfGenes1));
+        geneList.addAll(gen2.genes.subList(gen2.genes.size() - nrOfGenes2, gen2.genes.size()));
+        return new Genotype(geneList);
     }
 }
