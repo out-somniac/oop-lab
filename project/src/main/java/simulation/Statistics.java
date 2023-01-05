aeba9a26b9e58e168019d71858b002963d66edcf
Mikolaj Maslak <nicholasdev7@gmail.com>
2023-01-05 10:12:33 +0100

    added tracking of average lifespan

--- project/src/main/java/simulation/Statistics.java
+++ project/src/main/java/simulation/Statistics.java
@@ -1,17 +1,20 @@
 package simulation;
 
-public record Statistics(int dayNr, int nrOfAnimals, long animalsBorn, double averageEnergy, int plantCount) {
+public record Statistics(int dayNr, int nrOfAnimals, long animalsBorn, double averageEnergy, int plantCount, double lifespanOfDead) {
 
     @Override
     public String toString() {
         return """
-                ========================
                 day: %d overview
                 total animals: %d
                 animals born: %d
                 average energy: %.2f
                 total plants: %d
-                ========================
-                """.formatted(dayNr, nrOfAnimals, animalsBorn, averageEnergy, plantCount);
+                average lifespan of death animals: %.2f
+                """.formatted(dayNr, nrOfAnimals, animalsBorn, averageEnergy, plantCount, lifespanOfDead);
+    }
+
+    public String getCSVFormat() {
+        return "%d; %d; %d; %.2f; %d; %.2f".formatted(dayNr, nrOfAnimals, animalsBorn, averageEnergy, plantCount, lifespanOfDead);
     }
 }
